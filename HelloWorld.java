0dbd8a6935f4bdf2f7d936f427d42ed6badc5e65
Nguyễn Ngọc Lan Thi <thi.nguyen.set18@eiu.edu.vn>
2024-11-21 10:10:50 +0700

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Ngoc Lan Thi - 1234567890,");
+        System.out.println("Welcome to Github!");
+    }
+}
+
