18a08e35dbb143e6e7950a7134661d58882cd185
Nguyễn Ngọc Lan Thi <thi.nguyen.set18@eiu.edu.vn>
2024-11-22 08:19:43 +0700

    Add Main.java with welcome message

---
+++ Main.java
@@ -0,0 +1,26 @@
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello Nguyen Ngoc Lan Thi - 1234567890,");
+//         System.out.println("Welcome to Github!");
+//     }
+   
+// }
+
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Welcome, " + <your_name> + "!");
+//         System.out.println("Your student's ID is: " + <your_student_id>);
+//     }
+// }
+
+
+public class Main {
+    public static void main(String[] args) {
+       // Your code here
+    }
+}
+
+
